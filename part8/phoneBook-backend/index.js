d672f756fd00bcac687624c5ea228148f757d280
aishat001 <waheedaishat@gmail.com>
2021-08-04 23:50:50 +0100

    added part8 backend folder

--- part8/phoneBook-backend/index.js
+++ part8/phoneBook-backend/index.js
@@ -1,4 +1,6 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, gql, UserInputError } = require('apollo-server')
+
+const { v1: uuid } = require('uuid')
 
 let persons = [
   {
@@ -23,28 +25,58 @@ let persons = [
   },
 ]
 
+
+
 const typeDefs = gql`
+
   type Person {
     name: String!
     phone: String
     address: Address!
     id: ID!
   }
+  
   type Address {
     street: String!
     city: String! 
   }
+  enum YesNo {
+      YES
+      NO
+  }
+
   type Query {
     personCount: Int!
-    allPersons: [Person!]!
+    allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
   }
+
+  type Mutation {
+    addPerson(
+        name: String!
+        phone: String
+        street: String!
+        city: String!
+        ) : Person
+    editNumber(
+        name: String!
+        phone: String!
+    ): Person
+    }
 `
 
 const resolvers = {
+
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+        if (!args.phone) {
+            return persons
+        }
+        const byPhone = (Person) => 
+            args.phone === 'YES' ? Person.phone : !Person.phone
+            return persons.filter(byPhone)
+    },
     findPerson: (root, args) =>
       persons.find(p => p.name === args.name)
   },
@@ -55,7 +87,30 @@ const resolvers = {
         city: root.city
       }
     }
+  },
+  Mutation: {
+    addPerson: (root, args) => {
+        if (persons.find(p => p.name === args.name)) {
+            throw new UserInputError('name must be unique', {
+                invalidArgs: args.name,
+            })
+        }
+
+      const person = { ...args, id: uuid() }
+      persons = persons.concat(person)
+      return person
+    },
+    editNumber: (root, args) => {
+        const person = persons.find(p => p.name === args.name)
+        if (!person) {
+            return null
+        }
+        const updatedPerson = {...person, phone : args.phone}
+        persons = persons.map(p => p.name === args.name ? updatedPerson : p)
+        return updatedPerson
+    }
   }
+
 }
 
 const server = new ApolloServer({
