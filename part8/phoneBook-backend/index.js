e1ffa5c5cf41a20807bdef1b6a61de1acd7249b3
aishat001 <waheedaishat@gmail.com>
2021-09-01 15:13:02 +0100

    updated phonebookBackend

--- part8/phoneBook-backend/index.js
+++ part8/phoneBook-backend/index.js
@@ -195,10 +195,10 @@ const server = new ApolloServer({
     const auth = req ? req.headers.authorization : null
     if (auth && auth.toLowerCase().startsWith('bearer ')) {
       const decodedToken = jwt.verify(
-        auth.substring(7), JWT_SECRET
+        auth.substring(7), process.env.JWT_SECRET
       )
       const currentUser = await User.findById(decodedToken.id).populate('friends')
-      console.log(currentUser)
+      // console.log(currentUser)
       return { currentUser }
     }
   }
